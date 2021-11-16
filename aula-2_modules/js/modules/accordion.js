714991681f84c951bcf1e0066320e4c577a45c2f
unknown <arthurropke@hotmail.com>
2021-11-16 09:50:34 -0300

    aula02 modules

---
+++ aula-2_modules/js/modules/accordion.js
@@ -0,0 +1,18 @@
+export default function initAccordion() {
+	const accordionList = document.querySelectorAll(
+		'[data-anime="accordion"] dt'
+	)
+	const ativoClass = 'ativo'
+	if (accordionList.length) {
+		accordionList[0].nextElementSibling.classList.add(ativoClass)
+
+		function activeAccordion() {
+			this.classList.toggle(ativoClass)
+			this.nextElementSibling.classList.toggle(ativoClass)
+		}
+
+		accordionList.forEach(item => {
+			item.addEventListener('click', activeAccordion)
+		})
+	}
+}
